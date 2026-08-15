8ca47eb174072fae2ed3d21d1011c749c7732595
Miisan <128460054+Miisan-png@users.noreply.github.com>
2026-08-15 17:53:01 +0800

    red: adding an i tem rasies count

---
+++ ex00/tests.cpp
@@ -0,0 +1,10 @@
+#include "catch_amalgamated.hpp"
+#include "Inventory.hpp"
+
+TEST_CASE("adding an item raises the count") {
+    Inventory inv;
+
+    inv.add("potion");
+
+    CHECK(inv.count() == 1);
+}
