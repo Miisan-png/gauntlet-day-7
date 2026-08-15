873b014645b186ec1f9a31efcb3f99a1d27e47c0
Miisan <128460054+Miisan-png@users.noreply.github.com>
2026-08-15 17:56:28 +0800

    green raise count

---
+++ ex00/Inventory.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Inventory {
+public:
+    void add(const std::string& item);
+    int count() const;
+
+private:
+    std::vector<std::string> m_items;
+};
